0057d47302e567d0154282db4a5e4f568bf2adec
jean1024 <1737923789@qq.com>
2019-12-16 17:55:13 +0800

    add Readme

--- schedule.js
+++ schedule.js
@@ -2,9 +2,9 @@ const schedule = require('node-schedule')
 const getInfoAndSend = require('./app')
 function scheduleCronstyle(){
     const rule = new schedule.RecurrenceRule();
-    rule.dayOfWeek = [1, 2, 3, 4, 5, 6, 7];
-    rule.hour = 8;
-    rule.minute = 0;
+    rule.dayOfWeek = [1, 2, 3, 4, 5, 6, 7]; // 每周的哪天运行
+    rule.hour = 8;  // 哪个时辰运行
+    rule.minute = 0; // 分钟
 
     schedule.scheduleJob(rule, function(){
         console.log('scheduleCronstyle:' + new Date());
